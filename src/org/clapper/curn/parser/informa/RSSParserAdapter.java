dd3df5444f1f6d2a131a1e6146378acdaa0bcfc5
Brian Clapper <bmc@clapper.org>
2004-06-11 15:10:15 +0000

    Added more javadocs.

--- src/org/clapper/curn/parser/informa/RSSParserAdapter.java
+++ src/org/clapper/curn/parser/informa/RSSParserAdapter.java
@@ -19,8 +19,14 @@ import java.io.IOException;
 
 /**
  * This class implements the <tt>RSSParser</tt> interface and defines an
- * adapter for the <a href="http://informa.sourceforge.net/">Informa</a>
- * RSS Parser.
+ * adapter for the
+ * {@link <a href="http://informa.sourceforge.net/">Informa</a>}
+ * RSS Parser. Informa supports the
+ * {@link <a href="http://backend.userland.com/rss091">0.91</a>}, 0.92,
+ * {@link <a href="http://web.resource.org/rss/1.0/">1.0</a>} and
+ * {@link <a href="http://blogs.law.harvard.edu/tech/rss">2.0</a>} RSS formats,
+ * but does <i>not</i> support the
+ * {@link <a href="http://www.atomenabled.org/developers/">Atom</a>} format.
  *
  * @see org.clapper.rssget.parser.RSSParserFactory
  * @see org.clapper.rssget.parser.RSSParser
