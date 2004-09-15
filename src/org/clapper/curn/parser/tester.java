38a02f097f3a4bef545caaf1eed3cbf9779bb17d
Brian Clapper <bmc@clapper.org>
2004-09-15 20:13:59 +0000

    Now uses WordWrapWriter for output.

--- src/org/clapper/curn/parser/tester.java
+++ src/org/clapper/curn/parser/tester.java
@@ -26,12 +26,15 @@
 
 package org.clapper.curn.parser;
 
+import org.clapper.util.io.WordWrapWriter;
 import java.io.*;
 import java.util.*;
 import java.lang.reflect.Constructor;
 
 public class tester
 {
+    private static WordWrapWriter out = new WordWrapWriter (System.out);
+
     public static void main (String args[]) throws Throwable
     {
         if (args.length < 2)
@@ -47,9 +50,9 @@ public class tester
 
         for (int i = 1; i < args.length; i++)
         {
-            System.out.println ();
-            System.out.println (args[i] + ":");
-            System.out.println ();
+            out.println ();
+            out.println (args[i] + ":");
+            out.println ();
 
             FileInputStream is = new FileInputStream (args[i]);
             RSSChannel channel = parser.parseRSSFeed (is, null);
@@ -63,35 +66,44 @@ public class tester
     private static void show (RSSChannel channel)
         throws Throwable
     {
-        System.out.println ("Channel title: " + channel.getTitle());
-        System.out.println ("Channel link:  " + channel.getLink());
-        System.out.println ("RSS version:   " + channel.getRSSFormat());
-        System.out.print   ("Channel date:  ");
+        out.println ("Channel title: " + channel.getTitle());
+        out.println ("Channel link:  " + channel.getLink());
+        out.println ("RSS version:   " + channel.getRSSFormat());
+        out.print   ("Channel date:  ");
 
         Date date = channel.getPublicationDate();
         if (date != null)
-            System.out.println (date);
+            out.println (date);
         else
-            System.out.println ("<null>");
+            out.println ("<null>");
 
         for (Iterator it = channel.getItems().iterator(); it.hasNext(); )
         {
             RSSItem item = (RSSItem) it.next();
 
-            System.out.println ();
-            System.out.println ("Item title: " + item.getTitle());
-            System.out.println ("Item link:  " + item.getLink());
+            out.println ();
+            out.println ("Item title:  " + item.getTitle());
 
-            String s = item.getSummary();
-            if (s != null)
-                System.out.println ("Item desc:  " + s);
+            out.print ("Item author: ");
+            String s = item.getAuthor();
+            out.println ((s == null) ? "<null>" : s);
 
-            System.out.print ("Item date:  ");
+            out.println ("Item link:   " + item.getLink());
+
+            out.print ("Item date:   ");
             date = item.getPublicationDate();
+            s = "<null>";
             if (date != null)
-                System.out.println (date);
-            else
-                System.out.println ("<null>");
+                s = date.toString();
+            out.println (s);
+
+            s = item.getSummary();
+            if (s != null)
+            {
+                out.setPrefix ("Item desc:   ");
+                out.println (s);
+                out.setPrefix (null);
+            }
         }
     }
 }
