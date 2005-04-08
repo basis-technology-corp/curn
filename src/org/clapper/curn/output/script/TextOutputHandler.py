18e3e10364ce060d34d7c9cc253003ebc3099672
Brian Clapper <bmc@clapper.org>
2005-04-08 16:50:45 +0000

    Simplified slightly.

--- src/org/clapper/curn/output/script/TextOutputHandler.py
+++ src/org/clapper/curn/output/script/TextOutputHandler.py
@@ -24,175 +24,157 @@
  a pointer to or a copy of the original.
 """
 
+"""
+Python/Jython version of the curn-supplied C{TextOutputHandler}
+(org.clapper.curn.output.TextOutputHandler). This script must be used
+in conjunction with curn's ScriptOutputHandler class
+(org.clapper.curn.output.script.ScriptOutputHandler), which requires
+the presence of the Jakarta version of the Bean Scripting Framework (BSF).
+
+This script is intended primarily as a demonstration, though it is a
+fully-functioning script-based output handler.
+
+In addition to the configuration parameters supported by the
+ScriptOutputHandler class and its parent FileOutputHandler class, this
+script also honors an optional Message parameter. If present in the
+configuration section, the Message parameter defines an arbitrary
+string to be displayed at the top of the output.
+"""
+
 import sys
 from org.clapper.curn import CurnException
 from org.clapper.util.io import WordWrapWriter
 
-class TextOutputHandler:
+HORIZONTAL_RULE = "---------------------------------------" \
+                + "---------------------------------------"
+
+
+def processChannels():
     """
-    Python/Jython version of the curn-supplied C{TextOutputHandler}
-    (org.clapper.curn.output.TextOutputHandler). This script must be used
-    in conjunction with curn's ScriptOutputHandler class
-    (org.clapper.curn.output.script.ScriptOutputHandler), which requires
-    the presence of the Jakarta version of the Bean Scripting Framework (BSF).
-
-    This script is intended primarily as a demonstration, though it is a
-    fully-functioning script-based output handler.
-
-    In addition to the configuration parameters supported by the
-    ScriptOutputHandler class and its parent FileOutputHandler class, this
-    script also honors an optional Message parameter. If present in the
-    configuration section, the Message parameter defines an arbitrary
-    string to be displayed at the top of the output.
-    
+    Process the channels passed in through the Bean Scripting Framework.
     """
-    HORIZONTAL_RULE = "---------------------------------------" \
-                    + "---------------------------------------"
 
+    # If we didn't care about wrapping the output, we'd just use:
+    #
+    #     out = open (self.outputPath, "w")
+    #
+    # But it'd be nice to wrap long summaries on word boundaries at
+    # the end of an 80-character line. For that reason, we use the
+    # Java org.clapper.util.io.WordWrapWriter class.
 
-    def __init__ (self):
-        """
-        Initialize a new TextOutputHandler object.
-        """
-        self.__channels    = bsf.lookupBean ("channels")
-        self.__outputPath  = bsf.lookupBean ("outputPath")
-        self.__mimeTypeBuf = bsf.lookupBean ("mimeType")
-        self.__config      = bsf.lookupBean ("config")
-        self.__sectionName = bsf.lookupBean ("configSection")
-        self.__logger      = bsf.lookupBean ("logger");
-        self.__version     = bsf.lookupBean ("version")
-        self.__message     = None
+    out = WordWrapWriter (open (outputPath, "w"))
+    out.setPrefix ("")
 
-    def processChannels (self):
-        """
-        Process the channels passed in through the Bean Scripting Framework.
-        """
+    msg = config.getOptionalStringValue (sectionName, "Message", None)
 
-        # If we didn't care about wrapping the output, we'd just use:
-        #
-        #     out = open (self.__outputPath, "w")
-        #
-        # But it'd be nice to wrap long summaries on word boundaries at
-        # the end of an 80-character line. For that reason, we use the
-        # Java org.clapper.util.io.WordWrapWriter class.
+    totalNew = 0
 
-        out = WordWrapWriter (open (self.__outputPath, "w"))
-        out.setPrefix ("")
+    # First, count the total number of new items
 
-        msg = self.__config.getOptionalStringValue (self.__sectionName,
-                                                    "Message",
-                                                    None)
+    iterator = channels.iterator()
+    while iterator.hasNext():
+        channel_wrapper = iterator.next()
+        channel = channel_wrapper.getChannel()
+        totalNew = totalNew + channel.getItems().size()
 
-        totalNew = 0
+    if totalNew > 0:
+        # If the config file specifies a message for this handler,
+        # display it.
 
-        # First, count the total number of new items
+        if msg != None:
+            out.println (msg)
+            out.println ()
 
-        iterator = self.__channels.iterator()
+        # Now, process the items
+
+        indentation = 0
+        iterator = channels.iterator()
         while iterator.hasNext():
             channel_wrapper = iterator.next()
             channel = channel_wrapper.getChannel()
-            totalNew = totalNew + channel.getItems().size()
+            feed_info = channel_wrapper.getFeedInfo()
+            process_channel (out, channel, feed_info, indentation)
 
-        if totalNew > 0:
-            # If the config file specifies a message for this handler,
-            # display it.
+        mimeTypeBuf.append ("text/plain")
 
-            if msg != None:
-                out.println (msg)
-                out.println ()
+        # Output a footer
 
-            # Now, process the items
+        indent (out, indentation)
+        out.println ()
+        out.println (HORIZONTAL_RULE)
+        out.println (version)
+        out.flush()
 
-            indentation = 0
-            iterator = self.__channels.iterator()
-            while iterator.hasNext():
-                channel_wrapper = iterator.next()
-                channel = channel_wrapper.getChannel()
-                feed_info = channel_wrapper.getFeedInfo()
-                self.__process_channel (out, channel, feed_info, indentation)
+def process_channel (out, channel, feed_info, indentation):
+    """
+    Process all items within a channel.
+    """
+    logger.debug ("Processing channel \"" + str (channel.getLink()) + "\"")
 
-            self.__mimeTypeBuf.append ("text/plain")
+    # Print a channel header
 
-            # Output a footer
+    indent (out, indentation)
+    out.println (HORIZONTAL_RULE)
+    out.println (channel.getTitle())
+    out.println (channel.getLink().toString())
+    out.println (str (channel.getItems().size()) + " item(s)")
+    if config.showDates():
+        date = channel.getPublicationDate()
+        if date != None:
+            out.println (str (date))
 
-            self.__indent (out, indentation)
-            out.println ()
-            out.println (TextOutputHandler.HORIZONTAL_RULE)
-            out.println (self.__version)
-            out.flush()
+    if config.showRSSVersion():
+        out.println ("(Format: " + channel.getRSSFormat() + ")")
 
-    def __process_channel (self, out, channel, feed_info, indentation):
-        """
-        Process all items within a channel.
-        """
-        self.__logger.debug ("Processing channel \"" +
-                             str (channel.getLink()) + "\"")
+    indentation = indentation + 1
+    indent (out, indentation)
+    iterator = channel.getItems().iterator()
+    while iterator.hasNext():
+        # These are RSSItem objects
+        item = iterator.next()
 
-        # Print a channel header
+        out.println()
+        out.println (item.getTitle())
+        out.println (str (item.getLink()))
 
-        config = self.__config
-
-        self.__indent (out, indentation)
-        out.println (TextOutputHandler.HORIZONTAL_RULE)
-        out.println (channel.getTitle())
-        out.println (channel.getLink().toString())
-        out.println (str (channel.getItems().size()) + " item(s)")
         if config.showDates():
-            date = channel.getPublicationDate()
+            date = item.getPublicationDate();
             if date != None:
                 out.println (str (date))
 
-        if config.showRSSVersion():
-            out.println ("(Format: " + channel.getRSSFormat() + ")")
+        out.println()
 
-        indentation = indentation + 1
-        self.__indent (out, indentation)
-        iterator = channel.getItems().iterator()
-        while iterator.hasNext():
-            # These are RSSItem objects
-            item = iterator.next()
+        if not feed_info.summarizeOnly():
+            summary = item.getSummary()
+            if summary != None:
+                indent (out, indentation + 1)
+                out.println (summary)
+                indent (out, indentation)
 
-            out.println()
-            out.println (item.getTitle())
-            out.println (str (item.getLink()))
+def indent (out, indentation):
+    """
+    Apply a level of indentation to a WordWrapWriter, by changing
+    the WordWrapWriter's prefix string.
 
-            if config.showDates():
-                date = item.getPublicationDate();
-                if date != None:
-                    out.println (str (date))
+    out         - the org.clapper.util.io.WordWrapWriter
+    indentation - the numeric indentation level
+    """
 
-            out.println()
+    prefix = ""
+    for i in range (indentation):
+        prefix = prefix + "    "
 
-            if not feed_info.summarizeOnly():
-                summary = item.getSummary()
-                if summary != None:
-                    self.__indent (out, indentation + 1)
-                    out.println (summary)
-                    self.__indent (out, indentation)
-
-    def __indent (self, out, indentation):
-        """
-        Apply a level of indentation to a WordWrapWriter, by changing
-        the WordWrapWriter's prefix string.
-
-        out         - the org.clapper.util.io.WordWrapWriter
-        indentation - the numeric indentation level
-        """
-
-        prefix = ""
-        for i in range (indentation):
-            prefix = prefix + "    "
-
-        out.setPrefix (prefix)
+    out.setPrefix (prefix)
 
 # ---------------------------------------------------------------------------
 
-def main():
-    """
-    Main program entry point. This is what ScriptOutputHandler actually
-    ends up invoking.
-    """
-    handler = TextOutputHandler()
-    handler.processChannels()
+channels    = bsf.lookupBean ("channels")
+outputPath  = bsf.lookupBean ("outputPath")
+mimeTypeBuf = bsf.lookupBean ("mimeType")
+config      = bsf.lookupBean ("config")
+sectionName = bsf.lookupBean ("configSection")
+logger      = bsf.lookupBean ("logger");
+version     = bsf.lookupBean ("version")
+message     = None
 
-main()
+processChannels()
