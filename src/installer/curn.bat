2deb4e10da556fc3c3220d3d5875171323d511aa
Brian Clapper <bmc@clapper.org>
2005-12-23 22:09:07 +0000

    Fleshed out IzPack installer.

---
+++ src/installer/curn.bat
@@ -0,0 +1,11 @@
+@echo off
+if "%OS%" == "Windows_NT" setlocal
+rem ---------------------------------------------------------------------------
+rem Front end Windows script for the curn RSS reader
+rem
+rem $Id$
+rem ---------------------------------------------------------------------------
+
+"$JAVA_HOME\bin\java" -ea -client -classpath "$INSTALL_PATH\lib\ocutil.jar;$INSTALL_PATH\lib\xerces.jar;$INSTALL_PATH\lib\activation.jar;$INSTALL_PATH\lib\mail.jar;$INSTALL_PATH\lib\freemarker.jar;$INSTALL_PATH\lib\curn.jar" org.clapper.curn.Tool %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+:end
---
+++ src/installer/curn.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#
+# Front-end Unix shell script for the curn RSS reader
+#
+# $Id$
+# ---------------------------------------------------------------------------
+
+vm_opts=
+while [ $# -gt 0 ]
+do
+    case "$1" in
+        -D*|-X*)
+            vm_opts="$vm_opts $1"
+	    shift
+	    ;;
+        *)
+	    break
+	    ;;
+    esac
+done
+
+exec $JAVA_HOME/bin/java \
+-ea \
+-client \
+$vm_opts \
+-classpath \
+$INSTALL_PATH/lib/ocutil.jar:\
+$INSTALL_PATH/lib/xerces.jar:\
+$INSTALL_PATH/lib/activation.jar:\
+$INSTALL_PATH/lib/mail.jar:\
+$INSTALL_PATH/lib/freemarker.jar:\
+$INSTALL_PATH/lib/curn.jar \
+org.clapper.curn.Tool "${@}"
