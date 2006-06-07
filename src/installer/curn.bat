4a1b187493bd596d9c80c6f54c17a68c5fdfd960
Brian Clapper <bmc@clapper.org>
2006-06-07 00:41:23 +0000

    org.clapper.curn.home -> curn.home

--- src/installer/curn.bat
+++ src/installer/curn.bat
@@ -6,6 +6,6 @@ rem
 rem $Id$
 rem ---------------------------------------------------------------------------
 
-"$JAVA_HOME\bin\java" -classpath $INSTALL_PATH\lib\curnboot.jar -ea -client -Dorg.clapper.curn.home=$INSTALL_PATH org.clapper.curn.Bootstrap $INSTALL_PATH\lib $INSTALL_PATH\plugins @user.home\curn\plugins @user.home\.curn\plugins -- org.clapper.curn.Tool %1 %2 %3 %4 %5 %6 %7 %8 %9
+"$JAVA_HOME\bin\java" -classpath $INSTALL_PATH\lib\curnboot.jar -ea -client -Dcurn.home=$INSTALL_PATH org.clapper.curn.Bootstrap $INSTALL_PATH\lib $INSTALL_PATH\plugins @user.home\curn\plugins @user.home\.curn\plugins -- org.clapper.curn.Tool %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 :end 
