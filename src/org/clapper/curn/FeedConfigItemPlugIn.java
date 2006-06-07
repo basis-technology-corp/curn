741539b8c7d82cae4b7c92cc9cc1096e9523dadc
Brian Clapper <bmc@clapper.org>
2006-06-07 13:19:09 +0000

    OutputHandlerConfigItemPlugIn.runFeedConfigItemPlugIn() now returns a boolean. A "false" return value tells curn to skip the output handler.

--- src/org/clapper/curn/FeedConfigItemPlugIn.java
+++ src/org/clapper/curn/FeedConfigItemPlugIn.java
@@ -63,15 +63,18 @@ public interface FeedConfigItemPlugIn extends PlugIn
      *                     for the feed. The URL is guaranteed to be
      *                     present, but no other fields are.
      * 
+     * @return <tt>true</tt> to continue processing the feed,
+     *         <tt>false</tt> to skip it
+     *
      * @throws CurnException on error
      *
      * @see CurnConfig
      * @see FeedInfo
      * @see FeedInfo#getURL
      */
-    public void runFeedConfigItemPlugIn (String     sectionName,
-                                         String     paramName,
-                                         CurnConfig config,
-                                         FeedInfo   feedInfo)
+    public boolean runFeedConfigItemPlugIn (String     sectionName,
+                                            String     paramName,
+                                            CurnConfig config,
+                                            FeedInfo   feedInfo)
 	throws CurnException;
 }
