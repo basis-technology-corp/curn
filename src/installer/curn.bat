c91c400130a03df92d94ab228fb387f795e312da
Brian Clapper <bmc@clapper.org>
2006-11-02 21:54:48 +0000

    - Modified default Windows installation path from   "%SYSTEMDRIVE%\Program Files\clapper.org\curn" to   "%SYSTEMDRIVE%\Program Files\ClapperOrg\curn". The "." in "clapper.org"   was causing problems on Windows 2000.
    - Fixed Windows curn.bat file to quote arguments that might contains
      blanks.

--- src/installer/curn.bat
+++ src/installer/curn.bat
@@ -1,6 +1,5 @@
 @echo off
 if "%OS%" == "Windows_NT" setlocal
-
 rem ---------------------------------------------------------------------------
 rem Front end Windows script for the curn RSS reader
 rem
@@ -49,27 +48,14 @@ rem (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 rem THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 rem ---------------------------------------------------------------------------
 
-set JAVA_VM_ARGS=-Dcurn.home=$INSTALL_PATH
+set JAVA_VM_ARGS=-Dcurn.home="$INSTALL_PATH"
 
 rem Make sure Java user.home property accurately reflects home directory
-if NOT "%HOME%"=="" set JAVA_VM_ARGS=%JAVA_VM_ARGS% -Duser.home=%HOME%
+if NOT "%HOME%"=="" set JAVA_VM_ARGS=%JAVA_VM_ARGS% -Duser.home="%HOME%"
 
 if NOT "%CURN_JAVA_VM_ARGS%"=="" set JAVA_VM_ARGS=%JAVA_VM_ARGS% %CURN_JAVA_VM_ARGS%
 
-"$JAVA_HOME\bin\java" ^
-%JAVA_VM_ARGS% ^
--classpath "$INSTALL_PATH\lib\curnboot.jar" ^
--ea ^
--client ^
-org.clapper.curn.Bootstrap ^
-$INSTALL_PATH\lib ^
-$INSTALL_PATH\plugins ^
-@user.home\curn\plugins ^
-@user.home\.curn\plugins ^
-@user.home\curn\lib ^
-@user.home\.curn\lib ^
--- ^
-org.clapper.curn.Tool "%1" "%2" "%3" "%4" "%5" "%6" "%7" "%8" "%9"
+"$JAVA_HOME\bin\java" %JAVA_VM_ARGS% -classpath "$INSTALL_PATH\lib\curnboot.jar" -ea -client org.clapper.curn.Bootstrap "$INSTALL_PATH\lib" "$INSTALL_PATH\plugins" "@user.home\curn\plugins" "@user.home\.curn\plugins" "@user.home\curn\lib" "@user.home\.curn\lib" -- org.clapper.curn.Tool %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 :end
 
